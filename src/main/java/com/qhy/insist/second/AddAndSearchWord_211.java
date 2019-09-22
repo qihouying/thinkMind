78c7ca11cccc4329af228bb9aa1fb90435b6b777
dream <dream_0920@qq.com>
2019-09-22 14:55:49 +0800

    to do other things

--- src/main/java/com/qhy/insist/second/AddAndSearchWord_211.java
+++ src/main/java/com/qhy/insist/second/AddAndSearchWord_211.java
@@ -1,5 +1,10 @@
 package com.qhy.insist.second;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+
 /**
  * @Author houyingqi
  * @Date 2019-09-21 15:58
@@ -25,5 +30,56 @@ package com.qhy.insist.second;
  * You may assume that all words are consist of lowercase letters a-z.
  **/
 public class AddAndSearchWord_211 {
+    private TrieNode root = new TrieNode();
 
+    class TrieNode{
+        public TrieNode[] children = new TrieNode[26];
+        public boolean isLeaf;
+    }
+
+
+    public void addWord(String word) {
+        if (null == word || 0 == word.length())
+            return;
+        TrieNode node = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (null == node.children[c-'a']) {
+                node.children[c-'a'] = new TrieNode();
+            }
+            node = node.children[c-'a'];
+        }
+        node.isLeaf = true;
+    }
+
+
+    public boolean search(String word) {
+        if (null == word || 0 == word.length())
+            return false;
+        return isMatch(word.toCharArray(), 0, root);
+
+    }
+
+    public boolean isMatch(char[] word, int k, TrieNode node) {
+        if (k == word.length)
+            return node.isLeaf;
+        if (word[k] == '.') {
+            for (int i = 0; i < node.children.length; i++) {
+                if(null != node.children[i] && isMatch(word, k+1, node.children[i]))
+                    return true;
+            }
+        } else {
+            return (null != node.children[word[k]-'a'] && isMatch(word, k+1, node.children[word[k]-'a']));
+        }
+        return false;
+    }
+
+
+    public static void main(String[] args) {
+        AddAndSearchWord_211 wordDictionary = new AddAndSearchWord_211();
+        wordDictionary.addWord("word");
+        System.out.println(wordDictionary.search("pattern"));
+        System.out.println(wordDictionary.search("word"));
+        System.out.println(wordDictionary.search("w.rd"));
+    }
 }
---
+++ src/main/java/com/qhy/insist/second/ReplaceWords_648.java
@@ -0,0 +1,32 @@
+package com.qhy.insist.second;
+
+import java.util.List;
+
+/**
+ * @Author houyingqi
+ * @Date 2019-09-22 14:23
+ * @Description   [medium]
+ *
+ * In English, we have a concept called root, which can be followed by some other words to form another longer word
+ * - let's call this word successor. For example, the root an, followed by other, which can form another word another.
+ *
+ * Now, given a dictionary consisting of many roots and a sentence. You need to replace all the successor in the sentence
+ * with the root forming it. If a successor has many roots can form it, replace it with the root with the shortest length.
+ *
+ * You need to output the sentence after the replacement.
+ *
+ * Example 1:
+ *
+ * Input: dict = ["cat", "bat", "rat"]
+ * sentence = "the cattle was rattled by the battery"
+ * Output: "the cat was rat by the bat"
+ **/
+public class ReplaceWords_648 {
+    public String replaceWords(List<String> dict, String sentence) {
+
+    }
+
+    class TrieNode{
+        TrieNode[] children = new TrieNode[26];
+    }
+}
