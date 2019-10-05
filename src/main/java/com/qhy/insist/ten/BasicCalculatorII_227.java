1c3178697482f7842545cdf4dc678c8ccdb0e57e
dream <dream_0920@qq.com>
2019-10-06 00:15:59 +0800

    change

---
+++ src/main/java/com/qhy/insist/ten/BasicCalculatorII_227.java
@@ -0,0 +1,22 @@
+package com.qhy.insist.ten;
+
+/**
+ * @Author houyingqi
+ * @Date 2019-10-06 00:13
+ * @Description [Medium] Topics: [String]
+ *
+ * Implement a basic calculator to evaluate a simple expression string.
+ * The expression string contains only non-negative integers, + , - , * , / operators and
+ * empty spaces . The integer division should truncate toward zero.
+ * You may assume that the given expression is always valid.
+ * Some examples:
+ * "3+2*2" = 7
+ * " 3/2 " = 1
+ * " 3+5 / 2 " = 5
+ * Note: Do not use the eval built-in library function.
+ **/
+public class BasicCalculatorII_227 {
+    public int calculate(String s) {
+        
+    }
+}
